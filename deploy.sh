5571204110f31ae516e47dae6b8d610a72ab53f1
Abiola Ajibola <ajibolaabiolaahmed@gmail.com>
2026-07-17 12:27:52 +0100

    Check if deploy.sh has access to environment variables in CI environment

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 # Downloads assets for the map rendering and builds the app
+export
 git remote add origin https://github.com/abiola-ajibola/sst-raster-globe.git
 git lfs install
 git lfs fetch --include="assets.tar.gz"
