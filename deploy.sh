57bc55b8419c445c74cfcf025e108ebd979cd59c
Abiola Ajibola <ajibolaabiolaahmed@gmail.com>
2026-07-17 12:42:40 +0100

    fix: update deploy script to use environment variables

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,7 @@
 # Downloads assets for the map rendering and builds the app
-export
-git remote add origin https://github.com/abiola-ajibola/sst-raster-globe.git
+git remote add origin $GIT_ORIGIN
 git lfs install
-git lfs fetch --include="assets.tar.gz"
-git lfs checkout assets.tar.gz
-tar -xzf assets.tar.gz
-# ls -r ./public
+git lfs fetch --include="$LFS_FILENAME"
+git lfs checkout $LFS_FILENAME
+tar -xzf $LFS_FILENAME
 npm run build
